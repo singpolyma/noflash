8ee27c7af2b84119a437bc97da652dc02423dad7
Stephen Paul Weber <singpolyma@singpolyma.net>
2009-03-20 15:20:56 -0400

    Kill IE6

--- header.php
+++ header.php
@@ -1,3 +1,9 @@
+      <!--[if lt IE 7]>
+      <div id="ie6Warning">
+      <h2>Time to upgrade your browser</h2>
+      <p>If you're reading this, you're surfing using Internet Explorer 6, an eight-year-old browser that cannot cope with the demands of the modern internet. For the best web experience, we strongly recommend upgrading to <a href="http://www.google.com/chrome">Google Chrome</a>, <a href="http://www.getfirefox.com/">Firefox</a>, <a href="http://www.opera.com/">Opera</a>, <a href="http://www.apple.com/safari/">Safari</a>, or a more recent version of <a href="http://www.microsoft.com/windows/downloads/ie/getitnow.mspx">Internet Explorer</a>.</p>
+      </div>
+      <![endif]-->
 		<div id="header">
 			<?php if($LOGIN_DATA['user_id']) : ?>
 				<div id="actions"><a href="login/out.php">logout</a></div>
